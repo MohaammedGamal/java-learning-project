1c0bcd2165047c6692db15e1e4554161f8a301c5
MohaammedGamal <mohaammed.gamal@gmail.com>
2024-07-20 16:25:27 +0300

    Control structure - if else statements

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,17 @@
 public class Car {
 
+    int initial_speed, speed;
 
+    Car() {
+        this.initial_speed = 1;
+    }
+
+    public int get_speed() {
+        return this.speed;
+    };
+
+    public void set_speed(int s) {
+        this.speed = s;
+    };
 
 }
--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,18 @@ public class Main {
         String name = "Mohamed";
         String nick_name = "Gemy";
 
-        if (name == "Mohamed") {
+        if (name.equals("Mohamed"))
             System.out.println("Hello " + name + " i am from inside if statement ...");
-        }
 
+        Car mazda = new Car();
+        System.out.println("1 - Current speed is " + mazda.get_speed());
+        mazda.set_speed(260);
+        System.out.println("2 - Current speed is " + mazda.get_speed());
+        if (mazda.get_speed() > 250) {
+            System.out.print("Your current speed is ".toUpperCase() + mazda.get_speed() + " and you need to slow down ...".toUpperCase());
+        } else {
+            System.out.println("Your current speed is " + mazda.get_speed() + " and it's legal speed ...");
+        }
     }
 
 }
