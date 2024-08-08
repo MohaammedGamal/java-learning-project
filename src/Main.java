2be793e201567b13ca5bbda67f2b95202726c3c1
MohaammedGamal <mohaammed.gamal@gmail.com>
2024-08-08 16:19:09 +0300

    Clear

--- src/Main.java
+++ src/Main.java
@@ -9,47 +9,8 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
-        // For loops
-        int i = 0;
-        for (;;) {
-            System.out.println(i + " ) Hello " + i);
-            if (i < 5) {
-                System.out.println(i + " ) Hello " + i);
-            } else {
-                break;
-            }
-            i++;
-        }
 
-        for (int x = 1; x < 5; x++) {
-            for (int z = 0; z < 10; z++) {
-                System.out.print("*");
-            }
-            System.out.println("");
-        }
 
-        print_values(0);
-
-        // While loops
-        int counter = 0;
-        while(counter <= 5) {
-            System.out.println("I am a counter number " + counter + " from inside a while loop ...");
-            counter++;
-        }
-
-        do {
-            System.out.println("Test do ... while loop");
-        } while (0 > 1);
-
-    }
-
-    public static void print_values(int value) {
-        if (value <= 0)
-            return; // Here we used return to just go out and back to normal code out of the function
-
-        for (int y = value; y >= 0; y--) {
-            System.out.println("--) " + y);
-        }
     }
 
 }
