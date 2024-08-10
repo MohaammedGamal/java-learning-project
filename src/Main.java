ca2c61d25c76320348f8ed6684bf3e7b7a0244a7
MohaammedGamal <mohaammed.gamal@gmail.com>
2024-08-10 13:33:38 +0300

    Strings

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,28 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
 
+        char T = 'T';
+        char F = 'F';
+        char z = 'ز';
+
+        System.out.println(T);
+        System.out.println(F);
+        System.out.println(z);
+
+        String name = new String("Mohamed");
+//        String name = "Mohamed";
+        System.out.println(name.charAt(5));
+        System.out.println(name.length());
+
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println("Character number " + (i + 1) + " is " + name.charAt(i));
+        }
+
+//        if (name.equals("mohamed"))
+        if (name.equalsIgnoreCase("mohamed"))
+            System.out.println("======= Success =======");
+        else
+            System.out.println("======= Failed =======");
 
 
     }
