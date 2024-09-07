a0599e3fbca625f3c69304eaacc29ecb0e296e66
MohaammedGamal <mohaammed.gamal@gmail.com>
2024-09-07 13:03:57 +0300

    Some data types, end of beginners level

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,13 @@
 public class Car {
 
+    public Car() {
 
+    }
+
+    public void print_values(String [] values) {
+        for (String value : values) {
+            System.out.println("Value of values is " + value);
+        }
+    }
 
 }
